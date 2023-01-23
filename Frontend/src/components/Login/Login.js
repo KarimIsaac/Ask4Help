1e3a29b86098d2c065f4a93deff587c8f1e1285e
KarimIsaac <karim.isaac@lerniastudent.se>
2023-01-23 10:28:36 -0800

    2

--- Frontend/src/components/Login/Login.js
+++ Frontend/src/components/Login/Login.js
@@ -2,14 +2,13 @@ import './Login.css';
 import {  getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword   } from "firebase/auth";
 import { useState } from 'react';
 import app from './firebase-config';
-import {useNavigate} from "react-router-dom"
 
 
 function Login () {
   const auth = getAuth(app);
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
-  let navigate = useNavigate();
+  
   const signIn = () =>{   
   
   
@@ -49,12 +48,13 @@ const signUp = () => {
 
 
   return (
-    <div className='Sign-Google'>
+    
+    <div className='container'>
         <h1>SignIn with Email</h1>
         <input type={"email"} placeholder="Enter Email" onChange={(e) => setEmail(e.target.value)}/>
         <input type={"password"} placeholder="Enter Password" onChange={(e) => setPassword(e.target.value)}/>
-        <button onClick={signUp}>SignUp</button>
-        <button onClick={signIn}>SignIn</button>
+        <button className='btn-1' onClick={signUp}>SignUp</button>
+        <button className='btn-2' onClick={signIn}>SignIn</button>
     </div>
   )
 }

f4f358aca35ffb11fa3b87599c5f2055f866fad3
KarimIsaac <karim.isaac@lerniastudent.se>
2023-01-26 14:46:49 -0800

    fix

---
+++ Frontend/src/components/Login/firebase-config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDp7bIoaicXRd9SnxtgEn0EtT41PG1D4vs",
+    authDomain: "ask4help-ca30d.firebaseapp.com",
+    projectId: "ask4help-ca30d",
+    storageBucket: "ask4help-ca30d.appspot.com",
+    messagingSenderId: "36356695492",
+    appId: "1:36356695492:web:8bc1c2ed4aaf8acc773091",
+    measurementId: "G-W4WN03913J"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app;
+export const db = getFirestore(app);
--- Frontend/src/container/Header/NavBar.js
+++ Frontend/src/container/Header/NavBar.js
@@ -1,25 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import './NavBar.css'; 
 import Home from "../Home/Home";
 import CreatePost from "../../components/CreatePost/CreatePost";
 import Login from "../../components/Login/Login";
+import { auth } from "../../components/Login/firebase-config"
+import { signOut } from 'firebase/auth';
 
-function  NavBar()  {
+function NavBar() {
+  const [isAuth, setIsAuth] = useState(false);
+  
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      setIsAuth(true);
+    } else {
+      setIsAuth(false);
+    }
+  });
+
+  const signUserOut = () => {
+    signOut(auth).then(() => {
+      setIsAuth(false);
+      window.location.pathname ="/login";
+    });
+  };
+
   return (
     <Router>
-    <nav>
-      <Link to="/"> Home </Link>
-      <Link to="/login"> Login </Link>
-      <Link to="/createpost"> Create Post </Link>
-    </nav>
+      <nav>
+        <Link to="/">Home</Link>
+        {!isAuth ? ( 
+          <Link to="/login">Login</Link> 
+        ) : ( 
+          <button className='sign-out-btn' type='button' onClick={signUserOut}>Log out</button>
+        )}
+        <Link to="/createpost">Create Post</Link>
+      </nav>
 
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/createpost" element={<CreatePost />} />
-      <Route path="/login" element={<Login  />} />
-    </Routes>
-  </Router>
+      <Routes>
+      
+     
+        <Route path="/" element={<Home />} />
+        <Route path="/createpost" element={<CreatePost />} />
+        <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
+      </Routes>
+    </Router>
   )
 }
 
